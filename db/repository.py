df6a90f89c9544ba70e01d2624c73da5fb6cb659
AbdulRehman <abdulrehman21091999@gmail.com>
2026-04-05 16:09:14 +0530

    key problem

--- db/repository.py
+++ db/repository.py
@@ -10,7 +10,7 @@ class DocumentRepository:
                        INSERT INTO docuemts(
                            name , path , thumnail_path , tag , description , upload_date, lecture_date , total_pages
                        )values(?,?,?,?,?,?,?,?)
-                       """,(doc[1],doc[2],doc[3],doc[4],doc[5],doc[6],doc[7], doc[8]))
+                       """,(doc[0],doc[1],doc[2],doc[3],doc[4],doc[5],doc[6], doc[7]))
         connection.commit()
         connection.close()  
     def search_documents(self, tags= None, date=None):
